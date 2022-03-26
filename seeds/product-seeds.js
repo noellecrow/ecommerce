da3699e33aba75422fc4d769f60833c3c81f0446
noellecrow <crownoelle@gmail.com>
2022-03-26 10:58:58 -0700

    imported starter code

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -1,38 +1,39 @@
 const { Product } = require('../models');
 
 const productData = [
-    {
-        product_name: 'T-Shirt',
-        price: 19.99,
-        stock: 10,
-        category_id: 1,
-    },
-    {
-        product_name: 'Running Shorts',
-        price: 39.99,
-        stock: 8,
-        category_id: 2,
-    },
-    {
-        product_name: 'Purple Rain Prince Vinyl Record',
-        price: 29.99,
-        stock: 2,
-        category_id: 3,
-    },
-    {
-        product_name: 'Baseball Cap',
-        price: 14.99,
-        stock: 12,
-        category_id: 4,
-    },
-    {
-        product_name: 'Nike Metcons',
-        price: 139.99,
-        stock: 11,
-        category_id: 5,
-    },
+  {
+    product_name: 'Plain T-Shirt',
+    price: 14.99,
+    stock: 14,
+    category_id: 1,
+  },
+  {
+    product_name: 'Running Sneakers',
+    price: 90.0,
+    stock: 25,
+    category_id: 5,
+  },
+  {
+    product_name: 'Branded Baseball Hat',
+    price: 22.99,
+    stock: 12,
+    category_id: 4,
+  },
+  {
+    product_name: 'Top 40 Music Compilation Vinyl Record',
+    price: 12.99,
+    stock: 50,
+    category_id: 3,
+  },
+  {
+    product_name: 'Cargo Shorts',
+    price: 29.99,
+    stock: 22,
+    category_id: 2,
+  },
 ];
 
 const seedProducts = () => Product.bulkCreate(productData);
 
 module.exports = seedProducts;
+
